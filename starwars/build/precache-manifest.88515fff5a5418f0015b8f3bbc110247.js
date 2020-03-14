1efd2fab97e068170a18e2f7c1110acafb2c560a
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-03-14 20:31:38 +0100

    create production build

---
+++ starwars/build/precache-manifest.88515fff5a5418f0015b8f3bbc110247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c57e8a7e29c8c8b1cb08e50de81c9c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f48307f5b3fd6b59558",
+    "url": "/static/css/2.f26a4486.chunk.css"
+  },
+  {
+    "revision": "5c44ed64374a1d3ee7c3",
+    "url": "/static/css/main.af0b1e6f.chunk.css"
+  },
+  {
+    "revision": "8f48307f5b3fd6b59558",
+    "url": "/static/js/2.2ab93b43.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.2ab93b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c44ed64374a1d3ee7c3",
+    "url": "/static/js/main.d60ddea6.chunk.js"
+  },
+  {
+    "revision": "09bd293be4ae59bf78ba",
+    "url": "/static/js/runtime-main.80eae7cf.js"
+  }
+]);
