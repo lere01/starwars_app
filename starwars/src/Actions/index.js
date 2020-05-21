0797b6bb0d60f38d4f839d32c2d005341bea9ba4
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-05-21 21:22:12 +0100

    Bug: correct api endpoint

--- starwars/src/Actions/index.js
+++ starwars/src/Actions/index.js
@@ -1,19 +1,17 @@
 import Axios from "axios";
 
 export const getAll = () => dispatch => {
-  const url = "https://swapi.co/api/people";
+  const url = "https://swapi.dev/api/people/";
   Axios.get(url)
     .then(response => {
       dispatch({
         type: "UPDATE_ALL_PERSONS",
         payload: response.data.results
       });
-      
     })
     .catch(error => {
       console.log(error);
     });
-    
 };
 
 export const getPerson = async url => {
